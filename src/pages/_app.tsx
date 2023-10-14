683a6a0a7802092a1e302b58303bc9e37fb975e3
Luke Davis <russellldavis@me.com>
2023-10-14 15:19:43 -0500

    storybook, jest, prettier, postcss, mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { type AppType } from 'next/app';
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api';
 
-import "~/styles/globals.css";
+import '~/styles/globals.css';
+import { theme } from '../../theme';
+import { MantineProvider } from '@mantine/core';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +14,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
